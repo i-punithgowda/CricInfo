3b8ef38c6720baa3ed7bc779563ffb157b6fa106
Punith <impuni007@gmail.com>
2021-04-29 18:51:04 +0530

    Final commit , hopefully

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const getMatches = () => {
     xhr.addEventListener("readystatechange", function () {
         if (this.readyState === this.DONE) {
             getData(this.responseText)
+         
         }
     });
 
@@ -52,6 +53,7 @@ const builthtmlLive = (data) => {
     if(awayTeamBatting){
         liveHtml=
         `
+        <div class="matchno">${data.name}</div>
         <div class="team">${data.awayTeam.shortName}&nbsp;<span>${data.scores.awayScore}(${data.scores.awayOvers})</span>
         <div>${data.homeTeam.shortName}</div>
         <div class="status">${data.matchSummaryText}</div>
@@ -148,10 +150,91 @@ const pointsTable = () => {
     xhr.send(data);
 }
 
-pointsTable();
 
 function buildhtmlPtsTable(ptsData) {
-    console.log(JSON.parse(ptsData));
+    const data=JSON.parse(ptsData);
+    teamData=data.teams;
+   console.log(teamData)
+   let html=` <tr class="table">
+    <th>Team</th>
+    <th>Played</th>
+<th>Won</th>
+<th>Loss</th>
+<th>Nrr</th>
+<th>Logo</th>
+</tr>
+<div class="tableData">
+<tr>
+<td>${teamData[0].shortName}</td>
+<td>${teamData[0].played}</td>
+<td>${teamData[0].won}</td>
+<td>${teamData[0].lost}</td>
+<td>${teamData[0].netRunRate}</td>
+<td><img class="img" src=${teamData[0].logoUrl} alt=""></td>
+</tr>
+<tr>
+<td>${teamData[1].shortName}</td>
+<td>${teamData[1].played}</td>
+<td>${teamData[1].won}</td>
+<td>${teamData[1].lost}</td>
+<td>${teamData[1].netRunRate}</td>
+<td><img class="img" src=${teamData[1].logoUrl} alt=""></td>
+</tr>
+<tr>
+<td>${teamData[2].shortName}</td>
+<td>${teamData[2].played}</td>
+<td>${teamData[2].won}</td>
+<td>${teamData[2].lost}</td>
+<td>${teamData[2].netRunRate}</td>
+<td><img class="img" src=${teamData[2].logoUrl} alt=""></td>
+</tr>
+<tr>
+<td>${teamData[3].shortName}</td>
+<td>${teamData[3].played}</td>
+<td>${teamData[3].won}</td>
+<td>${teamData[3].lost}</td>
+<td>${teamData[3].netRunRate}</td>
+<td><img class="img" src=${teamData[3].logoUrl} alt=""></td>
+</tr>
+<tr>
+<td>${teamData[4].shortName}</td>
+<td>${teamData[4].played}</td>
+<td>${teamData[4].won}</td>
+<td>${teamData[4].lost}</td>
+<td>${teamData[4].netRunRate}</td>
+<td><img class="img" src=${teamData[4].logoUrl} alt=""></td>
+</tr>
+<tr>
+<td>${teamData[5].shortName}</td>
+<td>${teamData[5].played}</td>
+<td>${teamData[5].won}</td>
+<td>${teamData[5].lost}</td>
+<td>${teamData[5].netRunRate}</td>
+<td><img class="img" src=${teamData[5].logoUrl} alt=""></td>
+</tr>
+<tr>
+<td>${teamData[6].shortName}</td>
+<td>${teamData[6].played}</td>
+<td>${teamData[6].won}</td>
+<td>${teamData[6].lost}</td>
+<td>${teamData[6].netRunRate}</td>
+<td><img class="img" src=${teamData[6].logoUrl} alt=""></td>
+</tr>
+
+<tr>
+<td>${teamData[7].shortName}</td>
+<td>${teamData[7].played}</td>
+<td>${teamData[7].won}</td>
+<td>${teamData[7].lost}</td>
+<td>${teamData[7].netRunRate}</td>
+<td><img class="img" src=${teamData[7].logoUrl} alt=""></td>
+</tr>
+</div>
+`
+
+
+document.querySelector('.tbl').innerHTML=html;
+
 }
 
 let menu1=document.querySelector('.menu1');
@@ -160,10 +243,12 @@ let menu2=document.querySelector('.menu2');
 
 let menu1count=0;
 let menu2count=0;
+let menu3count=0;
 menu1.addEventListener('click',e=>{
     if(menu1count==0){
         document.querySelector('.completed').style.display="flex";
         document.querySelector('.upcoming').style.display="none";
+        document.querySelector('.ptsTbl').style.display="none";
         menu1count++;
     }else{
         document.querySelector('.completed').style.display="none";
@@ -177,6 +262,7 @@ menu2.addEventListener('click',e=>{
     if(menu2count==0){
         document.querySelector('.completed').style.display="none";
         document.querySelector('.upcoming').style.display="flex";
+        document.querySelector('.ptsTbl').style.display="none";
         menu2count++;
     }else{
         document.querySelector('.upcoming').style.display="none";
@@ -196,21 +282,18 @@ document.querySelector('.live').innerHTML=html;
 }
 
 
+let menu3=document.querySelector('.menu3');
+menu3.addEventListener('click',e=>{
+    if(menu3count==0){
+        document.querySelector('.ptsTbl').style.display="flex"
+        document.querySelector('.completed').style.display="none";
+        document.querySelector('.upcoming').style.display="none";
+        menu3count++;
+    }else{
+        document.querySelector('.ptsTbl').style.display="none";
+menu3count=0;
+    }
+})
 
 
-const data = null;
-
-    const xhr = new XMLHttpRequest();
-    xhr.withCredentials = true;
-
-    xhr.addEventListener("readystatechange", function () {
-        if (this.readyState === this.DONE) {
-            getData(this.responseText)
-        }
-    });
-
-    xhr.open("GET", "https://dev132-cricket-live-scores-v1.p.rapidapi.com/matches.php?upcomingLimit=30&inprogresslimit=5&completedlimit=50");
-    xhr.setRequestHeader("x-rapidapi-key", "49835d9c44msh21d22218a4ce348p184696jsnb30ba800d849");
-    xhr.setRequestHeader("x-rapidapi-host", "dev132-cricket-live-scores-v1.p.rapidapi.com");
-
-    xhr.send(data);
+pointsTable();
